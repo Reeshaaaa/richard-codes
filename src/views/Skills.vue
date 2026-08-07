0862afd7c1de5a62215f060808240f47d01757c8
Reeshaaaa <fousekrichard@gmail.com>
2026-08-06 22:57:14 -0400

    edited links

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -9,7 +9,7 @@
                 <hr />
                 <h1>Languages</h1>
                 <p><b>Czech</b> – My native language.</p>
-                <p><b>English (B2/C1)</b> – I learned English mostly online by immersion since I was a kid. School helped me structuralize it. Spending 3 months in USA helped me improve it.</p>
+                <p><b>English (B2/C1)</b> – I learned English mostly online by immersion since I was a kid. School helped me structuralize it. Spending 3 months in USA helped me improve it. In 2023, I recieved a CEFR B2 certificate.</p>
                 <p><b>Japanese (N4)</b> – Lower intermediate after 4 semesters of learning it in college.</p>
                 
                 <hr />
@@ -19,13 +19,14 @@
                 <ul>
                     <li>HTML, CSS, JavaScript</li>
                     <li>VueJS</li>
+                    <li>Node.js</li>
                     <li>MongoDB</li>
                     <li>Jest</li>
                     <li>Linux, AWS</li>
                     <li>Git</li>
                     <li>AI use for coding</li>
                 </ul>
-                <p>You can view my projects including this website on my <a href="https://github.com/Reeshaaaa">Github</a> page.</p>
+                <p>You can view my projects including this website on my <a href="https://github.com/Reeshaaaa" target="_blank" rel="noopener noreferrer">Github</a> page.</p>
             </div>
         </div>
     </div>
